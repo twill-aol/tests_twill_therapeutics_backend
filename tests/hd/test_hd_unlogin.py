4907674056016a5bedf4b6e0c13fd9ba5a8c97bf
Al-ta-iR <artyom.oleynik@gmail.com>
2022-09-19 19:09:18 +0200

    add step in test_hd_get_article to get article by human_url

--- tests/hd/test_hd_unlogin.py
+++ tests/hd/test_hd_unlogin.py
@@ -6,7 +6,6 @@ from lib.my_requests import MyRequests
 
 @allure.epic("[HD] No Authorization cases")
 class TestHDUnlogin(BaseCase):
-
     @allure.label("HD", "unlogin")
     @allure.description("This test checks /daily api")
     def test_hd_get_last_article(self):
@@ -73,7 +72,7 @@ class TestHDUnlogin(BaseCase):
 
     @allure.label("HD", "unlogin")
     @allure.description("This test checks /happifiers/[id] api")
-    def test_hd_get_article(self, human_url=''):
+    def test_hd_get_article(self, human_url=""):
         def asserts(response, uri_type, uri):
             with allure.step(f"Get article by {uri_type}: '{uri}'"):
                 Assertions.assert_code_status(response, 200),
@@ -123,22 +122,22 @@ class TestHDUnlogin(BaseCase):
                     ],
                 )
 
-        if human_url == '':
+        if human_url == "":
             for article_id in range(1, 450, 32):
                 response = MyRequests.get(f"/happifiers/{article_id}/")
                 if response.status_code != 200:
                     continue
                 elif response.status_code == 200:
-                    asserts(response, 'article_id', article_id)
+                    asserts(response, "article_id", article_id)
                     human_url = BaseCase.response_to_json(response)\
-                        ["human_url"]
+                    ["human_url"]
                     self.test_hd_get_article(human_url)
                     break
                 else:
                     assert 0, "None of the articles opened"
         else:
             response = MyRequests.get(f"/happifiers/{human_url}/")
-            asserts(response, 'human_url', human_url)
+            asserts(response, "human_url", human_url)
 
     @allure.label("HD", "unlogin")
     @allure.description("This test checks /happifiers+params api")
