36768da8892e5ce12e779f7eac3ff8d20dd2d4ac
Al-ta-iR <artyom.oleynik@gmail.com>
2022-09-19 19:05:54 +0200

    add step in test_hd_get_article to get article by human_url

--- tests/hd/test_hd_unlogin.py
+++ tests/hd/test_hd_unlogin.py
@@ -74,72 +74,71 @@ class TestHDUnlogin(BaseCase):
     @allure.label("HD", "unlogin")
     @allure.description("This test checks /happifiers/[id] api")
     def test_hd_get_article(self, human_url=''):
-        asserts = lambda response: (
-            Assertions.assert_code_status(response, 200),
-            Assertions.assert_json_has_keys(
-                response,
-                [
-                    "id",
-                    "ad_position",
-                    "human_url",
-                    "happifier_type",
-                    "content_type",
-                    "og_title",
-                    "og_description",
-                    "meta_title",
-                    "meta_description",
-                    "image_click_url",
-                    "image",
-                    "right_rail_image",
-                    "og_image",
-                    "title",
-                    "subtitle",
-                    "body",
-                    "credits",
-                    "video",
-                    "audio",
-                    "is_sponsored",
-                    "sponsor",
-                    "experts",
-                    "authors",
-                    "tags",
-                    "created_at",
-                    "modified_at",
-                    "publish_at_app",
-                    "rid",
-                    "author_image",
-                    "author_info",
-                    "author",
-                    "publisher_id",
-                    "publisher_image",
-                    "publisher_info",
-                    "publisher_name",
-                    "image_alt",
-                    "audio_duration",
-                    "publish_at",
-                    "audio_url",
-                    "video_url",
-                ],
-            )
-        )
+        def asserts(response, uri_type, uri):
+            with allure.step(f"Get article by {uri_type}: '{uri}'"):
+                Assertions.assert_code_status(response, 200),
+                Assertions.assert_json_has_keys(
+                    response,
+                    [
+                        "id",
+                        "ad_position",
+                        "human_url",
+                        "happifier_type",
+                        "content_type",
+                        "og_title",
+                        "og_description",
+                        "meta_title",
+                        "meta_description",
+                        "image_click_url",
+                        "image",
+                        "right_rail_image",
+                        "og_image",
+                        "title",
+                        "subtitle",
+                        "body",
+                        "credits",
+                        "video",
+                        "audio",
+                        "is_sponsored",
+                        "sponsor",
+                        "experts",
+                        "authors",
+                        "tags",
+                        "created_at",
+                        "modified_at",
+                        "publish_at_app",
+                        "rid",
+                        "author_image",
+                        "author_info",
+                        "author",
+                        "publisher_id",
+                        "publisher_image",
+                        "publisher_info",
+                        "publisher_name",
+                        "image_alt",
+                        "audio_duration",
+                        "publish_at",
+                        "audio_url",
+                        "video_url",
+                    ],
+                )
+
         if human_url == '':
             for article_id in range(1, 450, 32):
                 response = MyRequests.get(f"/happifiers/{article_id}/")
                 if response.status_code != 200:
                     continue
                 elif response.status_code == 200:
-                    with allure.step(f"Get article by article_id: '{article_id}'"):
-                        asserts(response)
-                    human_url = BaseCase.response_to_json(response)["human_url"]
+                    asserts(response, 'article_id', article_id)
+                    human_url = BaseCase.response_to_json(response)\
+                        ["human_url"]
                     self.test_hd_get_article(human_url)
                     break
                 else:
                     assert 0, "None of the articles opened"
         else:
             response = MyRequests.get(f"/happifiers/{human_url}/")
-            with allure.step(f"Get article by human_url: '{human_url}'"):
-                asserts(response)
-
+            asserts(response, 'human_url', human_url)
 
     @allure.label("HD", "unlogin")
     @allure.description("This test checks /happifiers+params api")
