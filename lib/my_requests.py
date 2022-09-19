bfd2b700ec429baeb63633d7957d9faa50449b54
Al-ta-iR <artyom.oleynik@gmail.com>
2022-09-19 14:05:31 +0200

    fix flake8

--- lib/my_requests.py
+++ lib/my_requests.py
@@ -5,27 +5,53 @@ from lib.logger import Logger
 
 class MyRequests:
     @staticmethod
-    def post(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
+    def post(
+        url: str,
+        data: dict = None,
+        headers: dict = None,
+        cookies: dict = None
+    ):
         with allure.step(f"POST request to URL '{url}'"):
             return MyRequests._send(url, data, headers, cookies, "POST")
 
     @staticmethod
-    def get(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
+    def get(
+        url: str,
+        data: dict = None,
+        headers: dict = None,
+        cookies: dict = None
+    ):
         with allure.step(f"GET request to URL '{url}'"):
             return MyRequests._send(url, data, headers, cookies, "GET")
 
     @staticmethod
-    def put(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
+    def put(
+        url: str,
+        data: dict = None,
+        headers: dict = None,
+        cookies: dict = None
+    ):
         with allure.step(f"PUT request to URL '{url}'"):
             return MyRequests._send(url, data, headers, cookies, "PUT")
 
     @staticmethod
-    def delete(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
+    def delete(
+        url: str,
+        data: dict = None,
+        headers: dict = None,
+        cookies: dict = None
+    ):
         with allure.step(f"DELETE request to URL '{url}'"):
             return MyRequests._send(url, data, headers, cookies, "DELETE")
 
     @staticmethod
-    def _send(url: str, data: dict, headers: dict, cookies: dict, method: str):
+    def _send(
+        url: str,
+        data: dict,
+        headers: dict,
+        cookies: dict,
+        method: str
+    ):
 
         url = f"https://therapeutics.stage-twill.health/api{url}"
         if headers is None:
@@ -36,13 +62,33 @@ class MyRequests:
         # Logger.add_request(url, data, headers, cookies, method)
 
         if method == "GET":
-            response = requests.get(url, params=data, headers=headers, cookies=cookies)
+            response = requests.get(
+                url,
+                params=data,
+                headers=headers,
+                cookies=cookies
+            )
         elif method == "POST":
-            response = requests.post(url, data=data, headers=headers, cookies=cookies)
+            response = requests.post(
+                url,
+                data=data,
+                headers=headers,
+                cookies=cookies
+            )
         elif method == "PUT":
-            response = requests.put(url, data=data, headers=headers, cookies=cookies)
+            response = requests.put(
+                url,
+                data=data,
+                headers=headers,
+                cookies=cookies
+            )
         elif method == "DELETE":
-            response = requests.delete(url, data=data, headers=headers, cookies=cookies)
+            response = requests.delete(
+                url,
+                data=data,
+                headers=headers,
+                cookies=cookies
+            )
         else:
             raise Exception(f"Bad HTTP method '{method}' was received")
 
