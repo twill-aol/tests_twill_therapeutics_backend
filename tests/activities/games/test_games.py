fdb721c9f7b89c47c161fc722c334ab8319bb276
Al-ta-iR <artyom.oleynik@gmail.com>
2022-09-29 11:44:18 +0200

    add test_games

--- tests/activities/games/test_games.py
+++ tests/activities/games/test_games.py
@@ -56,5 +56,5 @@ class TestKindnessChain(BaseCase):
         response_as_dict = self.response_to_json(response)
         points = response_as_dict["scores_old"]["points"]
         assert points > 0,\
-        f"Points of skill `{response_as_dict['scores_old']['name']}` \
-            = {points} it is < 0"
+        f"Points of skill `{response_as_dict['scores_old']['name']}` = \
+         {points} it is < 0"
