9180cf24e53dbb8060ed702d3b12118fd4c3d131
Al-ta-iR <artyom.oleynik@gmail.com>
2022-10-04 12:59:48 +0200

    add generate_names and good_phrases

--- lib/main_case.py
+++ lib/main_case.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import random
 from lib.assertions import Assertions
 from lib.base_case import BaseCase
 from lib.my_requests import MyRequests
@@ -29,13 +30,96 @@ class MainCase(BaseCase):
         return cookies
 
     @classmethod
+    def generate_names(self):
+        names = (
+            "Ethan",
+            "Kevin",
+            "Justin",
+            "Matthew",
+            "William",
+            "Christopher", 
+            "Anthony",
+            "Ryan",
+            "Nicholas",
+            "David",
+            "Alex",
+            "James",
+            "Josh",
+            "Dillon",
+            "Brandon",
+            "Philip",
+            "Fred",
+            "Tyler",
+            "Caleb",
+            "Thomas",
+            "Aaron",
+            "Brad",
+            "Emilу",
+            "Hannah",
+            "Natalie",
+            "Sophia",
+            "Ella",
+            "Madison",
+            "Sydney",
+            "Anna",
+            "Taylor",
+            "Isabella",
+            "Kayla",
+            "Victoria",
+            "Elizabeth",
+            "Ashley",
+            "Rachel",
+            "Alexis",
+            "Julia",
+            "Samantha",
+            "Haley",
+            "Olivia",
+            "Sarah",
+            "Jessica",
+            "Ava",
+            "Kaitlyn",
+            "Katherine"
+        )
+        surnames = (
+            "Johnson",
+            "Brown",
+            "Walker",
+            "Hall",
+            "White",
+            "Wilson",
+            "Thompson",
+            "Moore",
+            "Taylor",
+            "Anderson",
+            "Smith",
+            "Jackson",
+            "Harris",
+            "Martin",
+            "Young",
+            "Hernandez",
+            "Garcia",
+            "Davis",
+            "Miller",
+            "Martinez",
+            "Robinson",
+            "Clark",
+            "Rodrigues",
+            "Lewis",
+            "Lee",
+            "Allen",
+            "King"
+        )
+        names = f"{random.choice(names)} {random.choice(surnames)}"
+        return names
+
+    @classmethod
     def signup(self, email=None):
         dynamic_part = f'oleynik+{TIME_START}'
         domain = 'alarstudios.com'
         if email is None:
             email = f"{dynamic_part}@{domain}"
         signup_data = {
-                "username": f"Bot{TIME_START}",
+                "username": MainCase.generate_names(),
                 "email": email,
                 "password": 'Password+1',
                 "agreement": "on",
@@ -80,3 +164,30 @@ class MainCase(BaseCase):
             else:
                 break
         return text
+
+    @classmethod
+    def good_phrases(self):
+        phrases = (
+            "Be happy",
+            "Everything will be alright",
+            "Let's make this world a kinder place",
+            "Nothing is impossible",
+            "Believe in the dream",
+            "You can do more",
+            "Just do it",
+            "Smile and everything will work out",
+            "Focus on your breath",
+            "Feel the fluidity",
+            "We are all connected",
+            "Happiness comes from within",
+            "Our heart is full",
+            "Focus on your breath",
+            "Concentrate the mind on the present moment",
+            "Humility, infinity, integrity, liberty, majesty, synergy",
+            "Don't give up",
+            "Be an example",
+            "Stand on the side of the light",
+            "God is everywhere",
+        )
+        phrase = random.choice(phrases)
+        return phrase
--- tests/community/test_community.py
+++ tests/community/test_community.py
@@ -10,7 +10,7 @@ class TestCommunity(BaseCase):
     '''Tests Community posts'''
 
     user_id, email, cookies = MainCase.signup_router()
-    about_item_id = ""
+    post_id = ""
     offender_user_id = ""
 
     @allure.label("community", "post", "popular", "authorization")
@@ -25,22 +25,22 @@ class TestCommunity(BaseCase):
         Assertions.assert_code_status(response, 200)
         response_as_dict = BaseCase.response_to_json(response)
         if len(response_as_dict) > 0:
-            TestCommunity.about_item_id = response_as_dict[0]["id"]
+            TestCommunity.post_id = response_as_dict[0]["id"]
             TestCommunity.offender_user_id = response_as_dict[0]["user_id"]
 
     @allure.label("community", "post", "popular", "authorization")
     @allure.description("This test checks /api/report_abuse/ api")
     def test_report_post(self):
         '''Report post'''
-        if self.about_item_id != "":
-            params = {
+        if self.post_id != "":
+            data = {
                 "abuse_type": "post",
-                "about_item_id": self.about_item_id,
+                "about_item_id": self.post_id,
                 "offender_user_id": self.offender_user_id
             }
             response = MyRequests.post(
                 "/api/report_abuse/",
-                json=params,
+                json=data,
                 cookies=self.cookies
             )
             Assertions.assert_code_status(response, 200)
@@ -60,8 +60,8 @@ class TestCommunity(BaseCase):
         Assertions.assert_json_value_by_name(
             response,
             "about_item_id",
-            self.about_item_id,
-            f"Field `about_item_id` != {self.about_item_id}"
+            self.post_id,
+            f"Field `about_item_id` != {self.post_id}"
         )
         Assertions.assert_json_value_by_name(
             response,
@@ -92,7 +92,7 @@ class TestCommunity(BaseCase):
 
     @allure.label("community", "post", "followers", "authorization")
     @allure.description("This test checks \
-    //api/activity/?page=1&page_size=12&feed_filter=followers")
+    /api/activity/?page=1&page_size=12&feed_filter=followers")
     def test_community_followers_posts(self):
         '''Get followers posts in Community'''
         response = MyRequests.get(
@@ -103,3 +103,29 @@ class TestCommunity(BaseCase):
         response_as_dict = BaseCase.response_to_json(response)
         if len(response_as_dict) > 0:
             assert ("id" and "user_id") in response_as_dict[0]
+
+    @allure.label("community", "post", "comment", "authorization")
+    @allure.description("This test checks \
+    /api/activity_statuses/52650445/comments/")
+    def test_send_comment_to_post(self):
+        '''Send comment to post in Community'''
+        text = MainCase.good_phrases()
+        data = {
+            "text": text
+        }
+        response = MyRequests.post(
+            f"/api/activity_statuses/{self.post_id}/comments",
+            json=data,
+            cookies=self.cookies
+        )
+        Assertions.assert_code_status(response, 200)
+        Assertions.assert_json_value_by_name(
+            response,
+            "text",
+            text,
+            f"Comment is no {text}"
+        )
+
+# /api/activity_statuses/52651040/like/
+# /api/comments/433197/like/
+# post and delete empty data
