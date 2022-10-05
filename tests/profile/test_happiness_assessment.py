1959bf994b64601374de02ee586b1e534eeda03c
Al-ta-iR <artyom.oleynik@gmail.com>
2022-10-05 16:32:39 +0200

    fix test_ha_dismiss_status_after_complete

--- tests/profile/test_happiness_assessment.py
+++ tests/profile/test_happiness_assessment.py
@@ -28,6 +28,7 @@ class TestHA(BaseCase):
                 "dialog"
             ]
         )
+        TestHA.ha_id = BaseCase.response_to_json(response)["dialog"]["dialog_id"]
         return response
 
     @allure.label("ha", "assessment", "profile", "smoke", "authorization")
@@ -37,12 +38,11 @@ class TestHA(BaseCase):
         response = TestHA.test_ha_result(self)
         len_ha_res = len(BaseCase.response_to_json(response)["result"])
         assert len_ha_res == 0, "New user has HA-result"
-        self.ha_id = BaseCase.response_to_json(response)["dialog"]["dialog_id"]
         type_question = "single_obj"
         data = {"client_input": str(random.randint(1, 4))}
         while type_question != "end":
             response = MyRequests.post(
-                f"/api/assessment/v1/dialogs/{self.ha_id}/conversation/",
+                f"/api/assessment/v1/dialogs/{TestHA.ha_id}/conversation/",
                 json=data,
                 cookies=self.cookies
             )
@@ -57,7 +57,7 @@ class TestHA(BaseCase):
             conversation_id = \
                 BaseCase.response_to_json(response)["conversation_id"]
             data = {
-                "client_input": str(random.randint(1, 4)),
+                "client_input": str(random.randint(1, 3)),
                 "conversation_id": conversation_id
             }
             type_question = BaseCase.response_to_json(response)["data"]["type"]
@@ -70,10 +70,10 @@ class TestHA(BaseCase):
     def test_ha_dismiss_status_after_complete(self):
         '''Dismiss HA'''
         response = MyRequests.get(
-            f"/api/studies/dismiss-assessment/{self.ha_id}",
+            f"/api/studies/dismiss-assessment/{TestHA.ha_id}",
             cookies=self.cookies
         )
-        Assertions.assert_code_status(response, 200)
+        Assertions.assert_code_status(response, 202)
         Assertions.assert_json_value_by_name(
             response,
             "detail",
