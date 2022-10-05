9e6da687ac8dc79ea2d422002e441977c9b10e73
Al-ta-iR <artyom.oleynik@gmail.com>
2022-10-05 15:13:24 +0200

    add test_ha_result & test_ha_dismiss_status_after_complete

--- tests/profile/test_happiness_assessment.py
+++ tests/profile/test_happiness_assessment.py
@@ -9,30 +9,40 @@ from lib.my_requests import MyRequests
 @allure.epic("[Assessment] HA")
 class TestHA(BaseCase):
     '''Tests HA'''
-
     user_id, email, cookies = MainCase.signup_router()
+    ha_id = ""
 
-    @allure.label("ha", "assessment", "profile", "authorization")
-    @allure.description("This test checks \
-    /api/happiness_assessment/result/")
-    def test_ha_complete(self):
+    @allure.label("ha", "assessment", "profile", "smoke", "authorization")
+    @allure.description("This test checks get HA status")
+    def test_ha_result(self):
         '''Get HA status'''
-        def test_ha_result(self):
-            response = MyRequests.get(
-                "/api/happiness_assessment/result/",
-                cookies=self.cookies
-            )
-            Assertions.assert_code_status(response, 200)
-            return response
-        response = test_ha_result(self)
+        response = MyRequests.get(
+            "/api/happiness_assessment/result/",
+            cookies=self.cookies
+        )
+        Assertions.assert_code_status(response, 200)
+        Assertions.assert_json_has_keys(
+            response,
+            [
+                "result",
+                "dialog"
+            ]
+        )
+        return response
+
+    @allure.label("ha", "assessment", "profile", "smoke", "authorization")
+    @allure.description("This test checks complete HA")
+    def test_ha_complete(self):
+        '''Complete HA'''
+        response = TestHA.test_ha_result(self)
         len_ha_res = len(BaseCase.response_to_json(response)["result"])
         assert len_ha_res == 0, "New user has HA-result"
-        ha_id = BaseCase.response_to_json(response)["dialog"]["dialog_id"]
+        self.ha_id = BaseCase.response_to_json(response)["dialog"]["dialog_id"]
         type_question = "single_obj"
         data = {"client_input": str(random.randint(1, 4))}
         while type_question != "end":
             response = MyRequests.post(
-                f"/api/assessment/v1/dialogs/{ha_id}/conversation/",
+                f"/api/assessment/v1/dialogs/{self.ha_id}/conversation/",
                 json=data,
                 cookies=self.cookies
             )
@@ -51,6 +61,22 @@ class TestHA(BaseCase):
                 "conversation_id": conversation_id
             }
             type_question = BaseCase.response_to_json(response)["data"]["type"]
-        response = test_ha_result(self)
+        response = TestHA.test_ha_result(self)
         len_ha_res = len(BaseCase.response_to_json(response)["result"])
         assert len_ha_res > 0, "User doesn't have HA-result"
+
+    @allure.label("ha", "assessment", "profile", "smoke", "authorization")
+    @allure.description("This test checks dismiss HA after complete it")
+    def test_ha_dismiss_status_after_complete(self):
+        '''Dismiss HA'''
+        response = MyRequests.get(
+            f"/api/studies/dismiss-assessment/{self.ha_id}",
+            cookies=self.cookies
+        )
+        Assertions.assert_code_status(response, 200)
+        Assertions.assert_json_value_by_name(
+            response,
+            "detail",
+            "Not enough data. Dialogs for today: None.",
+            "Answer about dismiss assessment is wrong"
+        )
